5edb640073d605c8e6606dd4aa95e9ad9edb17d9
contributor <contributor@tethys-framework.de>
2020-12-22 20:31:44 +0100

    Install wizard: some css

--- install/Installer.php
+++ install/Installer.php
@@ -9,7 +9,11 @@
 namespace tt\install;
 
 use tt\core\Autoloader;
+use tt\core\page\Message;
+use tt\debug\Error;
 use tt\html\form\Form;
+use tt\html\form\FormfieldText;
+use tt\html\Html;
 use tt\service\Request;
 use tt\service\Templates;
 
@@ -20,26 +24,44 @@ class Installer {
 
 		if(!file_exists($file)){
 			require_once dirname(__DIR__).'/core/Autoloader.php';
-			require_once dirname(__DIR__).'/html/form/Form.php';
 			Autoloader::init();
 
 			if(!Request::cmd('createWebPointer')) {
-				$form = new Form("createWebPointer", "", "Create file");
-				//TODO:...
-				self::startWizard("File not found. $file", $form);
+				$form = new Form("createWebPointer", "", "Create init_web_pointer.php");
+				$suggest = "dirname(__DIR__).'/TTconfig/init_web.php'";
+				$form->addField(new FormfieldText("val_webpath", "Path to init_web.php", $suggest));
+				self::startWizard(
+					($m=new Message(Message::TYPE_INFO, "The file <b>$file</b> (excluded from the repo) should point to <b>init_web.php</b> (located in <a href='https://github.com/experder/TToolbox/blob/main/docs/folders.md'>CFG_DIR</a>)."))->toHtml()
+					. $form
+				);
 			}
 
-			Templates::create_file($file, __DIR__.'/templates/init_web_pointer.phpX', array(
-				"#INIT_WEB_PATH"=>"",//TODO:...
+			Templates::create_file($file, __DIR__.'/templates/init_web_pointer.php', array(
+				"#INIT_WEB_PATH"=>"require_once ".$_REQUEST["val_webpath"].";",
 			));
 		}
 
 		require_once $file;
 	}
 
-	public static function startWizard($hook, $wizard){
-		echo "=== INSTALL WIZARD ===<br>$hook<br>$wizard";
+	public static function startWizard($html){
+		echo self::wizardHtml($html);
 		exit;
+	}
+
+	public static function wizardHtml($body){
+		$css = file_get_contents(__DIR__."/wizard.css");
+		$head = "<style>$css</style>";
+		$head = "<head>$head</head>";
+
+		$html = "";
+		$html.=Html::H1("Install wizard");
+		$html.=$body;
+
+		$html = $head.$html;
+		$html = "<!DOCTYPE html><html>$html</html>";
+
+		return $html;
 	}
 
 }
